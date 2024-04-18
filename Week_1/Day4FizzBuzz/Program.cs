6e0871ec1504a640204f3b6c2c1692cc49ae6aec
Joshua Lee <joslee@geico.com>
2024-04-18 15:07:09 -0700

    "Organizing Folders"

--- Week_1/Day4FizzBuzz/Program.cs
+++ Week_1/Day4FizzBuzz/Program.cs
@@ -1,25 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-
-for (int x = 1; x <= 100;) 
+﻿internal class Program
 {
-    if (x % 3 == 0 && x % 5 == 0)
+    private static void Main(string[] args)
     {
-        Console.WriteLine("fizzbuzz");
+        for (int x = 1; x <= 100;)
+        {
+            if (x % 3 == 0 && x % 5 == 0)
+            {
+                Console.WriteLine("fizzbuzz");
+            }
+            if (x % 3 == 0)
+            {
+                Console.WriteLine("fizz");
+            }
+            else if (x % 5 == 0)
+            {
+                Console.WriteLine("buzz");
+            }
+            else
+            {
+                Console.WriteLine(x);
+            }
+            x++;
+        }
     }
-    if (x % 3 == 0)
-    {
-        Console.WriteLine("fizz");
-    }
-    else if (x % 5 == 0)
-    {
-        Console.WriteLine("buzz");
-    }
-    else 
-    {
-        Console.WriteLine(x);
-    }
-    x++;
 }
-
-
