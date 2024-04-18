5b30e7f89ac4f947b7d1c8e750e199e22a7337a1
Joshua Lee <joslee@geico.com>
2024-04-18 15:00:57 -0700

    "fizzbuzz ready"

--- Day4FizzBuzz/Program.cs
+++ Day4FizzBuzz/Program.cs
@@ -1,7 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 
-for (int x = 0; x <= 100; x++) 
+for (int x = 1; x <= 100;) 
 {
-    Console.WriteLine(x)
+    if (x % 3 == 0 && x % 5 == 0)
+    {
+        Console.WriteLine("fizzbuzz");
+    }
+    if (x % 3 == 0)
+    {
+        Console.WriteLine("fizz");
+    }
+    else if (x % 5 == 0)
+    {
+        Console.WriteLine("buzz");
+    }
+    else 
+    {
+        Console.WriteLine(x);
+    }
+    x++;
 }
+
+
