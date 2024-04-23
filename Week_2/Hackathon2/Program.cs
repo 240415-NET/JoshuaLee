f2d1139cc4b90c619f083fe077b0b5a0b590dfac
Joshua Lee <joslee@geico.com>
2024-04-23 07:30:22 -0700

    Backlog of Uploads

---
+++ Week_2/Hackathon2/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Week2Team2Hackathon;
+using System;
+using System.Collections.Generic;
+using Microsoft.VisualBasic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        bool quit = false; 
+        Console.WriteLine("Hello, Shopper! We're ready to begin your shopping list.");
+        
+
+        do 
+        {
+        string[] menuOptions = ["1. View grocery list", "2. Add to grocery list", "3. Exit"];
+        
+        
+        Console.WriteLine("Please select a menu number");
+        foreach(string options in menuOptions)
+            {
+                Console.WriteLine(options);
+            }
+        int userInput = Convert.ToInt32(Console.ReadLine().Trim());
+        string[] groceryList = new string[10];
+            if(userInput == 3)
+            {
+                quit = true;
+            }
+            else if(userInput == 2)
+            {
+                
+            }
+            else foreach(string groceries in groceryList)
+            {
+                Console.WriteLine(groceries);
+            }
+        
+        }  
+        while (quit == false); 
+    }
+}
---
+++ Week_2/ListDictionary/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace ListDictionary;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<string> cityList = new List<string>();
+
+        cityList.Add("Miami");
+        cityList.Add("Tampa");        
+        cityList.Add("Sarasota");
+        cityList.Add("Chicago");
+        cityList.Add("Clearwater");
+        
+        foreach(string city in cityList)
+        {
+            Console.WriteLine(city);
+        }
+
+        Dictionary<string, string> simplePets = new Dictionary<string, string>();
+        simplePets.Add("Jonathan", "Ellie");
+        simplePets.Add("Ross", "Brodie");
+        simplePets.Add("Mike", "Carl");
+        simplePets.Add("Marcus", "Ziggy");
+
+        //foreach (KeyValuePair<string, string> household in simplePets)
+        //This is an example of implicity typing using "var" in C#
+
+        foreach (var household in simplePets)
+        {
+            Console.WriteLine(household.GetType());
+            Console.WriteLine(household.Key + " owns " + owner.Value);
+        }
+
+        Dictionary<string, List<string>> petDictionary = new();
+
+        //This is an example of nesting collections. You can nest a dictionary within a dictionary, if needed.
+        //Instead of creating my lists somewhere else, I can create them while adding to my dictionary
+
+        List<string> jonPets = new List<string>(){"Ellie", "Pancake"};
+
+        petDictionary.Add("Jonathan", jonPets);
+        petDictionary.Add("Ross", new List<string>(){"Brody"});
+        petDictionary.Add("Mike", new List<string>(){"Carl"});
+        petDictionary.Add("Marcus", new List<string>(){"Ziggy", "Luna", "Amelia", "Pyewacket", "Love Muffin"});
+
+        foreach(var person in petDictionary)
+        {
+            Console.WriteLine(person.Key + person.Value);
+        }
+    }
+}
